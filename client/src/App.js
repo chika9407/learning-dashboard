38af48a20e7e41bdb0078518c031bc259192076c
Rosanne Kruithof <rosannekruithof@gmail.com>
2020-10-21 20:38:19 +0200

    made navbar separate component

--- client/src/App.js
+++ client/src/App.js
@@ -3,44 +3,32 @@ import "./App.css";
 import AddCourse from "./components/AddCourse";
 import CoursePage from "./components/CoursePage";
 import NavBar from "./components/NavBar";
-import { Switch, Route, Link, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import HomePage from "./components/HomePage";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/addcourse">
-                  Add course
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
+      <Router>
+        <NavBar />
         <div className="app container text-center p-4">
           <Switch>
             <Route path="/addcourse">
               <AddCourse />
             </Route>
-
             <Route path="/courses/:id" component={CoursePage} />
-
             <Route path="/">
               <HomePage />
             </Route>
           </Switch>
         </div>
-      </div>
+      </Router>
     );
   }
 }
